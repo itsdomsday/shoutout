05def4b36ba3bd1d96396ec1f500e1ca07925adb
DavidQuebengco <118255286+DavidQuebengco@users.noreply.github.com>
2023-02-07 06:02:22 +0800

    partial layout done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,58 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+@section('title', 'home')
+@section('content')
+
+        <div>
+            @if(session('success'))
+            <div class="alert alert-primary" role="alert">
+            {{session('success')}}
+            </div>
+            @endif
+            <div class="card p-3 mb-3">
+                <p class="fw-bold">Write a Twat</p>
+                <form action="" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="row">
+                        <div class="col-10" id="imageArea">
+                            <input type="hidden" name="user_id" value="">
+                            <input class="form-control" placeholder="Say anything you want..." type="text" name="content">
+                            
+                            <input class="form-control d-none" type="file" name="image" id="image" accept=".gif,.jpg,.jpeg,.png" onchange="imageUpload(event);">
+                            <label class="mt-2 bg-primary text-white rounded px-2 py-1 fw-bold" for="image" style="cursor: pointer;" id="imageUploadLabel"><small>⬆ Add a photo...</small></label>
+                        </div>
+                        <div class="col-2">
+                            <button type="submit" class="btn btn-primary">
+                                Shout
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h2>Recent Shouts</h2>
+                    <div class="card my-2">
+                        <div class="card-body">
+                            <h6 class="fw-bold">
+                                <a href="" style="text-decoration:none">David</a>
+                               
+                                <a href="#" class="float-end dropdown-toggle" style="text-decoration:none" data-bs-toggle="dropdown"></a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="">Delete</a></li>
+                                </ul>
+                                
+                                <small class="text-muted float-end mx-3 fw-normal">⏲</small>
+                            </h6>
+                            <p>
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis consectetur vel placeat ducimus blanditiis illum fugiat nam quibusdam ratione dicta maiores esse, quos laborum voluptates, libero pariatur possimus magnam odio!
+                            </p>
+                        </div>    
+                     </div>
+                </div>    
             </div>
         </div>
-    </div>
-</x-app-layout>
+
+
+ @endsection           
+                          
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,29 +7,186 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- css -->
+        <link rel="stylesheet" href="C:\xampp\htdocs\shoutout\resources\css\style.css">
+
+        <!-- icons -->
+        <link rel="stylesheet" href="https://kit.fontawesome.com/03e59edb08.css" crossorigin="anonymous">
+
+        <!-- bootstrap -->
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <style>
+     *{
+      margin: 0;
+      padding: 0;
+      text-decoration: none;
+    }
+
+    body{
+      box-sizing: border-box;
+    }
+
+    .sidebar{
+      position: sticky;
+      background: #e0e0e0;
+      width: 250px;
+      height: 100%;
+      top: 0;
+      left: 0;
+      z-index: 1;
+    }
+    
+    .sidebar ul li {
+      margin: 25px 0;
+    }
+    
+    .sidebar ul li h4{
+      color: #000000;
+      margin-left: 20px;
+      transition: 0.4s;
+    }
+
+    .sidebar h2{
+      margin-left: 20px;
+    }
+
+    .main{
+      margin-left: 220px;
+    }
+  
+    #child{
+      margin-left: 250px;
+      margin-top: -70px;
+      border-radius: 50%;
+    }
+
+  .main{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    flex-flow: wrap;
+  }
+        </style>
+
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="sidebar col-2 position-sticky">
+                    <a class="navbar-brand" href="#">
+                        <img src="https://w7.pngwing.com/pngs/227/383/png-transparent-franklin-marshall-college-youtube-logo-youtube-blue-text-trademark.png" height="65" width="85" style="margin-left: 10px;   border-radius: 50%;" alt=""
+                        loading="lazy" style="border-radius: 50%" />
+                    </a>
+                    <strong style="float: right; margin-top: 18px; margin-right: 5px"><h1>ShoutOut!</h1></strong> 
+                    </br>
+                    </br>
+                    <h2>Places</h2>
+                    <ul class="nav">
+                    <li>
+                        <a href="#">
+                        <i class="fas fa-server"></i>
+                        <span><h4>Friends</h4></span>
+                        </a>
+                    </li>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+                    <li>
+                        <a href="#">
+                        <i class="fas fa-server"></i>
+                        <span><h4>Marketplace</h4></span>
+                        </a>
+                    </li>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                    <li>
+                        <a href="#">
+                        <i class="fas fa-server"></i>
+                        <span><h4>Groups</h4></span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                        <i class="fas fa-server"></i>
+                        <span><h4>Most Recent</h4></span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                        <i class="fas fa-server"></i>
+                        <span><h4>Events</h4></span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                        <i class="fas fa-server"></i>
+                        <span><h4>Favorites</h4></span>
+                        </a>
+                    </li>
+                    </ul>
+
+
+                    <h2>Menu</h2>
+                    <ul class="nav">
+                    <li>
+                        <a href="#">
+                        <i class="fas fa-server"></i>
+                        <span><h4>Statistics</h4></span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                        <i class="fas fa-server"></i>
+                        <span><h4>Dashboard</h4></span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                        <i class="fas fa-server"></i>
+                        <span><h4>Documents</h4></span>
+                        </a>
+                    </li>
+                    </ul>
+
+                    
+                    <h2>Other</h2>
+                    <ul class="nav">
+                    <li>
+                        <a href="#">
+                        <i class="fas fa-server"></i>
+                        <span><h4>Settings</h4></span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                        <i class="fas fa-server"></i>
+                        <span><h4>Log Out</h4></span>
+                        </a>
+                    </li>
+                    </ul>
+                </div>
+                <div class="col-7 mt-5">
+                    <main>
+                      @yield('content')
+                    </main>
+                </div>
+              </div>    
         </div>
+    
+
+        
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/03e59edb08.js" crossorigin="anonymous"></script>
     </body>
 </html>
