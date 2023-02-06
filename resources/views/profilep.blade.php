8d02ff5204987d676c22ac3653495e8e308fd071
shirone Fortuno <118256204+shironeF@users.noreply.github.com>
2023-02-07 04:42:02 +0800

    added images for profile album

--- resources/views/profilep.blade.php
+++ resources/views/profilep.blade.php
@@ -19,7 +19,7 @@
 
     .sidebar{
       position: fixed;
-      background: gray;
+      background: #e0e0e0;
       width: 250px;
       height: 100%;
       top: 0;
@@ -44,6 +44,19 @@
     .main{
       margin-left: 220px;
     }
+  
+    #child{
+      margin-left: 250px;
+      margin-top: -70px;
+      border-radius: 50%;
+    }
+
+  .main{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    flex-flow: wrap;
+  }
   </style>
 </head>
 <body>
@@ -144,22 +157,22 @@
       </ul>
       </div>
       
-    <div class="main">
+      <div class="main">
     <main style="margin-top: -5px;">
   <div class="container pt-1"></div>
   <div style="float: left; margin: 40px ;">
           <img src="https://cdn4.iconfinder.com/data/icons/facebook-and-social-media-2/64/Facebook_and_Social_Media-11-512.png" height="50" alt="">
-          <h3 style="float: right; margin: 10px;">Friends </h3>
+          <h3 style="float: right; margin: 10px;">Weekly Friends </h3>
         </div>
 
         <div style="float: left; margin: 40px ;">
           <img src="https://cdn-icons-png.flaticon.com/512/2977/2977815.png" height="45" alt="">
-           	<h3 style="float: right; margin: 10px;">Followers </h3>
+           	<h3 style="float: right; margin: 10px;">Weekly Followers </h3>
             </div>
         
         <div style="float: left; margin: 40px;">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJOIdtka4kzZFal6HYSh-EUFxiJ5TeVTSKSkoJ4BOt_5qRmBZW38RMyVrHwrP9128XKAE&usqp=CAU" height="55" alt="">
-           <h3 style="float: right; margin: 10px;">Following </h3>
+           <h3 style="float: right; margin: 10px;">Weekly Following </h3>
         </div>
         
         <div style="float: left; margin: 40px;">
@@ -176,10 +189,28 @@
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUAMLwfxVieHHK6Bv_CX1X41OW-RkZxoxuE3yAAH6fjuQEeDHQUh3dfkIrtPZFg_VBJ8o&usqp=CAU" height="55" alt="">
            <h3 style="float: right; margin: 10px; margin-left: 5px">Stories </h3>
         </div>
+</div>
 </main>
 
-<div class="container">hello</div>
-    </div>
-    
+
+<div style="float: left; margin: 40px;">
+       <a href=""><img src="https://scontent.fcrk4-1.fna.fbcdn.net/v/t1.6435-9/134156035_3679434545448073_7107774170773165511_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFo2PavscfqpCSE5kVbyZm_eYtrk1plQTN5i2uTWmVBMyaM9n2wybEbHRib9VL4M1fICRx93AYEAv6c7VOcMkUj&_nc_ohc=pT9fpkk9xaIAX806Vp8&tn=jpUEIVEff-V9z0JH&_nc_ht=scontent.fcrk4-1.fna&oh=00_AfDE9ILTCfjMvOcawulUWhVTqUFHe7z7vJqUtwmc80hxwQ&oe=6408D696" alt="" height="100" id="child"></a> 
+       <div><strong><h3 style="margin-left: 250px;">Shirone Fortuno</h3></strong></div>  
+      
+
+      <div style="float: left; margin-left: 250px;">
+      Post
+      Followers
+      Following</div>
+      </div>
+
+      <div class="album" style="float: left;">
+      <a href="https://scontent.fcrk4-1.fna.fbcdn.net/v/t1.6435-9/129314832_3612613648796830_6724460720046237658_n.jpg?stp=dst-jpg_p960x960&_nc_cat=103&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGspCHYIBfe6x5hQIxcYXOC0gu2MzgbvBPSC7YzOBu8E_1d6DFLgMcah40D4C9rcQf0Cg1vWIi0Ot7uBSd8Z9xo&_nc_ohc=I2I9tziR494AX90hINq&_nc_ht=scontent.fcrk4-1.fna&oh=00_AfADIpg_T7T3GvDZDG9ECXgshNR61M1pd7kzcourQa8FyA&oe=6408CF16"><img src="https://scontent.fcrk4-1.fna.fbcdn.net/v/t1.6435-9/129314832_3612613648796830_6724460720046237658_n.jpg?stp=dst-jpg_p960x960&_nc_cat=103&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeGspCHYIBfe6x5hQIxcYXOC0gu2MzgbvBPSC7YzOBu8E_1d6DFLgMcah40D4C9rcQf0Cg1vWIi0Ot7uBSd8Z9xo&_nc_ohc=I2I9tziR494AX90hINq&_nc_ht=scontent.fcrk4-1.fna&oh=00_AfADIpg_T7T3GvDZDG9ECXgshNR61M1pd7kzcourQa8FyA&oe=6408CF16" height="300"  alt=""></a>
+      <a href="https://scontent.fcrk4-1.fna.fbcdn.net/v/t1.6435-9/61497496_2287926681265540_8510780303239806976_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeHNZ6N24_SChU47XIPbi_ODcaEyyVUQ-8hxoTLJVRD7yC4Oob2-QHqMQpGsCx9--NP88GsKQ6yf9Z0Qtmr8TvVe&_nc_ohc=dOEl6xIsEhgAX9JjsLh&tn=jpUEIVEff-V9z0JH&_nc_ht=scontent.fcrk4-1.fna&oh=00_AfB7ANVXptBpgrR3rqPx1HA8rPQQFk-RlqSiZyijwgI3Gg&oe=6408E257"><img src="https://scontent.fcrk4-1.fna.fbcdn.net/v/t1.6435-9/61497496_2287926681265540_8510780303239806976_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeHNZ6N24_SChU47XIPbi_ODcaEyyVUQ-8hxoTLJVRD7yC4Oob2-QHqMQpGsCx9--NP88GsKQ6yf9Z0Qtmr8TvVe&_nc_ohc=dOEl6xIsEhgAX9JjsLh&tn=jpUEIVEff-V9z0JH&_nc_ht=scontent.fcrk4-1.fna&oh=00_AfB7ANVXptBpgrR3rqPx1HA8rPQQFk-RlqSiZyijwgI3Gg&oe=6408E257" height="300" alt=""></a>
+      <a href="https://scontent.fcrk4-1.fna.fbcdn.net/v/t1.6435-9/58763350_2238447429546799_3979491966604804096_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeHyPyt9Lr3xNYnhztXR56NY2laFc3qNOEPaVoVzeo04Q4MwjxIRh2Cl1UovJ7VzLZFGuyIboWGP9vwbqcUvYf-c&_nc_ohc=QVSkkx9o3w0AX92xsUT&_nc_ht=scontent.fcrk4-1.fna&oh=00_AfBciiKP3hUz5W3btXbKPHGyIhk8WncQaQY8FrPwbtKIAQ&oe=6408D3D9"><img src="https://scontent.fcrk4-1.fna.fbcdn.net/v/t1.6435-9/58763350_2238447429546799_3979491966604804096_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeHyPyt9Lr3xNYnhztXR56NY2laFc3qNOEPaVoVzeo04Q4MwjxIRh2Cl1UovJ7VzLZFGuyIboWGP9vwbqcUvYf-c&_nc_ohc=QVSkkx9o3w0AX92xsUT&_nc_ht=scontent.fcrk4-1.fna&oh=00_AfBciiKP3hUz5W3btXbKPHGyIhk8WncQaQY8FrPwbtKIAQ&oe=6408D3D9" height="300" alt=""></a>
+      <a href="https://scontent.fcrk4-1.fna.fbcdn.net/v/t31.18172-8/22459478_1527645933960289_8849104674690725844_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeFNLQ4j3Or0u14zYtS1BsCQ51YHtLfzzhPnVge0t_POEwGRV7z_XTlfQSy0aFhMgPW8ZbsoSDQl-5jTptMDLAS5&_nc_ohc=XKlTupjNE2cAX8tJor3&_nc_oc=AQkRJ9coZ2QgIGDkMogCHPb1vZGIsH6AAY7lVPMThmzJ2Aq74wUDk-u8WjoUYuD93DU&_nc_ht=scontent.fcrk4-1.fna&oh=00_AfADX93B4OgesddmzaTb22xk5JR84GCoyCwPEYMiP77UrQ&oe=6408EC4F"><img src="https://scontent.fcrk4-1.fna.fbcdn.net/v/t31.18172-8/22459478_1527645933960289_8849104674690725844_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=19026a&_nc_eui2=AeFNLQ4j3Or0u14zYtS1BsCQ51YHtLfzzhPnVge0t_POEwGRV7z_XTlfQSy0aFhMgPW8ZbsoSDQl-5jTptMDLAS5&_nc_ohc=XKlTupjNE2cAX8tJor3&_nc_oc=AQkRJ9coZ2QgIGDkMogCHPb1vZGIsH6AAY7lVPMThmzJ2Aq74wUDk-u8WjoUYuD93DU&_nc_ht=scontent.fcrk4-1.fna&oh=00_AfADX93B4OgesddmzaTb22xk5JR84GCoyCwPEYMiP77UrQ&oe=6408EC4F" height="300" alt=""></a>
+      </div>
+
+      <div class="album" style="float: left;">About</div>
 </body>
 </html>
