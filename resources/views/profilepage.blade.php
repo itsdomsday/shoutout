68b5a825abb242951235e355284b8f9557847d26
shirone Fortuno <118256204+shironeF@users.noreply.github.com>
2023-02-11 03:51:46 +0800

    adding profile page body content

--- resources/views/profilepage.blade.php
+++ resources/views/profilepage.blade.php
@@ -21,10 +21,10 @@
         }
 
         body{
-            background: whitesmoke;
+            background: lightblue;
         }
 
-        /* sidebar */
+/* sidebar */
 #wrapper {
   padding-left: 0;
   -webkit-transition: all 0.5s ease;
@@ -58,7 +58,14 @@
   padding: 15px;
 }
 #wrapper.toggled #page-content-wrapper {
-    padding-left: 40px;
+    padding-left: 20px;
+}
+
+#toplinks a:link,
+#toplinks a:focus{
+    text-decoration: none;
+    font-size: 20px;
+    color: black;
 }
 /* Sidebar Styles */
 
@@ -121,25 +128,102 @@
     margin-right: 0;
   }
 }
-        #wrapper #sidebar-wrapper .profile h4{
-            color: white;
-            text-align: center;
-            margin: 10px 0 5px;
+#wrapper #sidebar-wrapper .profile h4{
+color: white;
+text-align: center;
+margin: 10px 0 5px;
+}
+#wrapper #sidebar-wrapper .profile p{
+color: rgb(206, 204, 253);
+text-align: center;
+font-size: 14px;
+}
+#wrapper #sidebar-wrapper .profile img{
+display: block;
+width: 100px;
+height: 100px;
+border-radius: 50%;
+margin: 0 auto;
+margin-top: 10px;
+}
 
-        }
-        #wrapper #sidebar-wrapper .profile p{
-            color: rgb(206, 204, 253);
-            text-align: center;
-            font-size: 14px;
-        }
-        #wrapper #sidebar-wrapper .profile img{
-            display: block;
-            width: 100px;
-            height: 100px;
-            border-radius: 50%;
-            margin: 0 auto;
-            margin-top: 10px;
-        }
+
+/* Top Nav Style*/      
+.topnav {
+  overflow: hidden;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav .search-container {
+  float: left;
+}
+
+.topnav input[type=text] {
+  padding: 6px;
+  margin-top: 8px;
+  font-size: 17px;
+  border: none;
+}
+
+.topnav .search-container button {
+  float: right;
+  padding: 6px 10px;
+  margin-top: 8px;
+  margin-right: 16px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+
+.topnav .search-container button:hover {
+  background: #ccc;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav .search-container {
+    float: none;
+  }
+  .topnav a, .topnav input[type=text], .topnav .search-container button {
+    float: none;
+    display: block;
+    text-align: left;
+    width: auto;
+    margin: 0;
+    padding: 14px;
+  }
+  .topnav input[type=text] {
+    border: 1px solid #ccc;  
+  }
+}
+/*Profile body*/
+#pbleft{
+  float: left;
+  width: 50%
+} 
+#pbright{
+  float: left;
+  width: 50%
+} 
+
+#postsection{
+  background: white;
+  border-radius: 10px;
+  width: 500px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
     </style>
 </head>
 <body>
@@ -158,15 +242,15 @@
                 </ul>
                 <li><a href="#"> 
                     <span><i class="fa-solid fa-message"></i></span>
-                    <span>Messages</span></a></li>
+                    <span> Messages</span></a></li>
                 
                 <li><a href="#">
                     <span><i class="fa-solid fa-signs-post"></i></span>
-                    <span>Posts</span></a></li>
+                    <span> Posts</span></a></li>
 
                 <li><a href="#">
                     <span><i class="fa-regular fa-image"></i></span>
-                    <span>Gallery</span></a></li>
+                    <span> Gallery</span></a></li>
             </ul>
 
             <ul class="sidebar-nav">
@@ -174,15 +258,15 @@
 
                 <li><a href="#"> 
                     <span><i class="fa-solid fa-tv"></i></span>
-                    <span>Watch Videos</span></a></li>
+                    <span> Watch Videos</span></a></li>
 
                 <li><a href="#">
                     <span><i class="fa-solid fa-music"></i></span>
-                    <span>Listen to Music</span</a></li>
+                    <span> Listen to Music</span</a></li>
 
                 <li><a href="#">
                     <span><i class="fa-solid fa-podcast"></i></span>
-                    <span>Podcast</span></a></li>
+                    <span> Podcast</span></a></li>
             </ul>
 
             <ul class="sidebar-nav">
@@ -190,15 +274,15 @@
 
                 <li><a href="#"> 
                     <span><i class="fa-solid fa-user-group"></i></span>
-                    <span>Friends</span></a></li>
+                    <span> Friends</span></a></li>
 
                 <li><a href="#">
                     <span><i class="fa-solid fa-users"></i></span>
-                    <span>Followers</span></a></li>
+                    <span> Followers</span></a></li>
 
                 <li><a href="#">
                     <span><i class="fa-solid fa-user-plus"></i></span>
-                    <span>Following</span></a></li>
+                    <span> Following</span></a></li>
             </ul>
 
             <ul class="sidebar-nav">
@@ -206,7 +290,7 @@
 
                <li><a href="#">
                     <span><i class="fa-solid fa-store"></i></span>
-                    <span>Marketplace</span></a></li>
+                    <span> Marketplace</span></a></li>
             </ul>
 
             <ul class="sidebar-nav">
@@ -214,36 +298,75 @@
                 
                     <li><a href="#">
                     <span><i class="fa-solid fa-gear"></i></span>
-                    <span>Settings</span></a></li>
+                    <span> Settings</span></a></li>
                     
                     <li><a href="#">
                     <span><i class="fa-solid fa-sliders"></i></span>
-                    <span>Others</span></a></li>
+                    <span> Others</span></a></li>
                     
                     <li><a href="#">
                     <span><i class="fa-solid fa-right-from-bracket"></i></span>
-                    <span>Log Out</span></a></li>
+                    <span> Log Out</span></a></li>
             </ul>
         </div>
 
         <div id="page-content-wrapper">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-lg-12">
-                        <a href="#" class="btn" id="menu-toggle"><img src="https://img.freepik.com/free-vector/illustrationn-megaphone-monochrome-style-isolated-white-background_1284-38767.jpg" style="border-radius: 50%; width: 50px; height: 50px;" alt=""></a></div>
-                        <div class="col-xs-12 col-md-2">
-                        </div>
-                    
-                        <div class="container-fluid">
-                                    <div class="row">
-                                    <div class="col-xs-12 col-md-6" style="background: lightblue;"><h3>Profile</h3>
-                                </div>
-                                <div class="col-xs-12 col-md-6">
-                                    <div>
-                                <iframe class="ratio ratio-4x3" style="width: 100%; height: 100%;" src="https://www.youtube.com/embed/Cj3AV92fJ90"></iframe>
-                                    </div>
-                                </div>
+                    <div class="col-lg-1">
+                        <a href="#" class="btn" id="menu-toggle">
+                            <img src="https://img.freepik.com/free-vector/illustrationn-megaphone-monochrome-style-isolated-white-background_1284-38767.jpg" style="border-radius: 50%; width: 50px; height: 50px;" alt="">
+                        </a>
                     </div>
+
+                    <!--top nav/ searchbar-->
+                      <div class="col-lg-3" id="toplinks">
+                        <div class="topnav">
+                          <div class="search-container">
+                              <form action="/action_page.php">
+                                <input type="text" placeholder="Search.." name="search">
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                              </form>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="col-lg-3" id="toplinks">
+                          <a href="#" style="color: black;">
+                              <span><i class="fa-solid fa-people-group"></i></span>
+                                  <span>Groups</span>
+                          </a>    
+                      </div>
+                                 
+                      <div class="col-lg-3" id="toplinks">
+                          <a href="#" style="color: black;">
+                              <span><i class="fa-solid fa-gamepad"></i></span>
+                                  <span>Gaming</span>
+                          </a>    
+                      </div>
+                     
+                      <div class="col-lg-2" id="toplinks">
+                          <a href="#" style="color: black;">
+                              <span><i class="fa-sharp fa-solid fa-cloud-sun"></i></span>
+                                  <span>Stories</span>
+                          </a>
+                      </div>
+                      <!--top nav/ searchbar-->
+<!--profilebody content-->
+<div id="pbleft">
+    <div class="wrapper" id="postsection">
+      <form action="" method="post" class="form">
+        <textarea name="message" id="" cols="30" rows="10" class="message" placeholder="What's on your mind?" style="text: italic;"></textarea>
+        <button type="submit" class="btn">Post</button>
+      </form> 
+
+    </div>
+</div>
+<div id="pbright">
+  <iframe class="ratio ratio-4x3" style="background: black; width: 100%; height: 500px; border-radius: 5%;" src="https://www.youtube.com/embed/iQbpwSCMcF4"></iframe>
+  <iframe class="ratio ratio-4x3" style="background: black; width: 100%; height: 500px; border-radius: 5%;" src="https://www.youtube.com/embed/Cj3AV92fJ90"></iframe>
+</div>        
+                                  
             </div>
         </div>
    </div> 
