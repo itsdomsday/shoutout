3eaeeb945ae2ccdb79c79ed0179c080e8a98a485
itsdomsday <dgvcristobal@gmail.com>
2023-02-11 20:03:27 +0800

    Updated Routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/profilep', function () {
+    return view('profilep');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
