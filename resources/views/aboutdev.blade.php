1459680049158fe39d69f2eb757f1931a965d579
shirone Fortuno <118256204+shironeF@users.noreply.github.com>
2023-02-13 03:08:43 +0800

    creating shoutout about page

--- resources/views/aboutdev.blade.php
+++ resources/views/aboutdev.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Document</title>
+    <title>ShoutOut</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
         *{
